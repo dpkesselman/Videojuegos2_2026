3d99b6c95bc1662796e7247159e02975bcc230a3
Daniela Kesselman <d.p.kesselman@gmail.com>
2026-09-03 17:32:37 -0300

    Top Down animation click

--- Assets/Scripts/TopDown/PlayerTopDown.cs
+++ Assets/Scripts/TopDown/PlayerTopDown.cs
@@ -12,6 +12,7 @@ public class PlayerTopDown : MonoBehaviour
     [SerializeField] private float left;
     [SerializeField] private float right;
     [SerializeField] private float bottom;
+    [SerializeField] private float top;
 
 
     void Start()
@@ -27,9 +28,12 @@ public class PlayerTopDown : MonoBehaviour
         float vertical = Input.GetAxis("Vertical");
         movementInput = new Vector2(horizontal, vertical).normalized;
 
-        animator.SetFloat("movX", Mathf.Abs(horizontal)); // Llamamos al parámetro float del Animator en el eje X 
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, left, right), Mathf.Clamp(transform.position.y, bottom, top), transform.position.z);
 
-        transform.position = new Vector3(Mathf.Clamp(transform.position.x, left, right), Mathf.Clamp(transform.position.y, bottom, 0), transform.position.z);
+        if(Input.GetMouseButtonDown(0))
+        {
+           animator.SetTrigger("Shoot"); 
+        }        
     }
 
 
