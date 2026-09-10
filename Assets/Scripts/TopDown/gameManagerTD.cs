723605f295dd12e61031fe8cc6c56ffb6fbb710a
Daniela Kesselman <d.p.kesselman@gmail.com>
2026-09-10 18:21:34 -0300

    camera size and resolution

---
+++ Assets/Scripts/TopDown/gameManagerTD.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class gameManagerTD : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // Switch to 640 x 480 full-screen
+        Screen.SetResolution(640, 1080, true);
+    }
+}
+
